2950b0faaae5c0a6260ed3fab82cf9ffd04f1252
betul.yanal <betul.yanal@ibb.gov.tr>
2023-06-01 14:21:14 +0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens:{
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px'
+      }
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+      colors: {
+        'tlw-red': '#BC1A45',
+        'tlw-melon': '#FFD369',
+        'tlw-grey': '#DDDDDD',
+        'tlw-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 }
